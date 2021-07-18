1c37823709bd021a16901b9ec925e512d894760e
R-adachi <nisizakakkz@gmail.com>
2021-07-19 08:00:22 +0900

    test commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     keys = fkeys.readlines()
     consumer_key = keys[0]
     consumer_secret = keys[1]
-    access_token= keys[2]
+    access_token= keys[2
     access_token_secret = keys[3]
     return
 
