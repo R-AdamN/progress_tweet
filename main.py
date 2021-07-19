39df6f60906eade1f8aaab32cb84ac261cb5692c
R-adachi <nisizakakkz@gmail.com>
2021-07-19 20:21:51 +0900

    make function sum_word_count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tweepy
+from pathlib import Path
 
 def main():
     # テキストファイルからキー類読み込み
@@ -8,13 +9,28 @@ def main():
     consumer_secret_key = keys[1].replace('\n','')
     access_token= keys[2].replace('\n','')
     access_token_secret = keys[3].replace('\n','')
+    fkeys.close()
 
     # オブジェクト生成
     auth = tweepy.OAuthHandler(consumer_key,consumer_secret_key)
     auth.set_access_token(access_token,access_token_secret)
     api = tweepy.API(auth)
 
-    
+
+    # パス読み込み
+    fpath = open('./secret/path.txt', 'r')
+    path = fpath.read().replace('\n','')
+    fpath.close()
+
+    # ファイル開き
+    p = Path(path)
+    files = p.glob("*.txt")
+
+    # 文字数カウント
+    sum_word_count = 0
+    for file in files:
+        with file.open() as f:
+            sum_word_count += len(f.read())
 
     return
 
