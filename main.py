a831cc277ba51cf3bdfe1d7da5687efd7d7b9f4a
R-adachi <nisizakakkz@gmail.com>
2021-07-19 20:03:32 +0900

    make api object

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import tweepy
 
 def main():
+    # テキストファイルからキー類読み込み
     fkeys = open('./secret/keys2.txt', 'r')
     keys = fkeys.readlines()
-    consumer_key = keys[0]
-    consumer_secret = keys[1]
-    access_token= keys[2
-    access_token_secret = keys[3]
+    consumer_key = keys[0].replace('\n','')
+    consumer_secret_key = keys[1].replace('\n','')
+    access_token= keys[2].replace('\n','')
+    access_token_secret = keys[3].replace('\n','')
+
+    # オブジェクト生成
+    auth = tweepy.OAuthHandler(consumer_key,consumer_secret_key)
+    auth.set_access_token(access_token,access_token_secret)
+    api = tweepy.API(auth)
+
+    
+
     return
 
 if __name__ == '__main__':
