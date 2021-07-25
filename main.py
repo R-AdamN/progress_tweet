eb5f28166f0e198321da627981979bafedfbfcb9
R-adamN <nisizakakkz@gmail.com>
2021-07-25 18:18:10 +0900

    ひとまず完成

--- main.py
+++ main.py
@@ -1,8 +1,56 @@
 import tweepy
 from pathlib import Path
 import os
+import datetime
+import time
 
 def main():
+
+    while(True):
+        # 日付が変わったか
+        if(day_change()):
+            # API作成
+            api = make_api()
+            # 進捗取得
+            progress = get_progress()
+            if(progress != None):
+                progress_txt = "本日の進捗は"+str(progress)+"文字です!"
+                api.update_status(progress_txt)
+                print(progress_txt)
+        # 1時間に一回実行
+        time.sleep(3600)
+
+
+# 日付が変わったか確認
+def day_change():
+    # 現在時刻取得
+    dt_now = datetime.datetime.now()
+    # 日付を文字列で抜き出し
+    str_today = dt_now.strftime('%Y-%m-%d')
+
+    fday_path = './secret/day.txt'
+
+    # 日付変更フラグ
+    day_is_change = 0
+
+    if(os.path.exists(fday_path)):
+        fday = open(fday_path, 'r')
+        # 機能の日付を取得
+        yesterday_date = fday.read().replace('\n','')
+        yesterday_datetime = datetime.datetime.strptime(yesterday_date, '%Y-%m-%d')
+        fday.close()
+        # 機能の日付+1日の0時以降か
+        if(dt_now>=yesterday_datetime+datetime.timedelta(days=1)):
+            day_is_change = 1
+
+    # 今日の日付を書き込み
+    fday = open(fday_path, 'w')
+    fday.write(str_today)
+    fday.close()
+    return day_is_change
+
+
+def make_api():
     # テキストファイルからキー類読み込み
     fkeys = open('./secret/keys2.txt', 'r')
     keys = fkeys.readlines()
@@ -17,10 +65,7 @@ def main():
     auth.set_access_token(access_token,access_token_secret)
     api = tweepy.API(auth)
 
-    progress = get_progress()
-    if(progress != None):
-        print(progress)
-
+    return api
 
 
 def get_progress():
